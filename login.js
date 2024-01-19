35417f2fd62729116b423b71cbfe648eaca757a8
Tywayah Bryson <Tbryson1024@gmail.com>
2024-01-19 13:18:22 -0600

    slideable signin/signup

--- login.js
+++ login.js
@@ -1,17 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var form = document.getElementById('signupForm');
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-        // Get form inputs
-        var username = document.getElementById('username').value;
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
-        var confirmPassword = document.getElementById('confirmPassword').value;
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
 
-        // Log the user inputs
-        console.log('Username:', username);
-        console.log('Email:', email);
-        console.log('Password:', password);
-        console.log('Confirm Password:', confirmPassword);
-
-        // Additional logic (e.g., send data to the server using AJAX)
-    });
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
