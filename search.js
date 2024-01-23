7a2a0779acf859e5186837eaa4222ccfbcf72610
Jennifer Larsen <djlarsen1999@gmail.com>
2024-01-23 00:52:02 -0600

    working on api integration and node.js

--- search.js
+++ search.js
@@ -1,7 +1,29 @@
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
+
+const express = require('express');
+const app = express();
+const port = 3000; // choose any available port
+
+// Define a route
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
+
 const performSearchButton = document.getElementById('performSearch');
 const searchInput = document.getElementById('searchInput');
 const ingredientsInput = document.getElementById('ingredientsInput');
 const searchResultsContainer = document.getElementById('searchResults');
+
+fetch('https://api.edamam.com/api/recipes/v2?type=public&app_id=7e56dd2d&app_key=685541325d49119c9bfd59298f854231')
+  .then((res) => res.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
 
 performSearchButton.addEventListener('click', () => {
     performSearch();
@@ -40,3 +62,11 @@ function displayResults(results) {
         searchResultsContainer.appendChild(ul);
     }
 }
+
+
+/*Application ID
+7e56dd2d
+
+Application Keys
+cab991bd72c7cc71873919863969a51e	—
+*/
