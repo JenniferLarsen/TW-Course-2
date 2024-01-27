6bd6fbff81ac3ae5a8b128dc0d7ec80c52f4997c
Jennifer Larsen <djlarsen1999@gmail.com>
2024-01-26 19:26:56 -0600

    Ingredient search is working, category/selection, and recipe are not.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Server-side JavaScript
-const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
+const path = require("path");
 
 const app = express();
 const port = 3000;
@@ -11,43 +11,51 @@ app.use(express.json());
 app.use(cors());
 
 // Import and configure dotenv to load environment variables from .env file
-require('dotenv').config();
+require("dotenv").config();
 
 // Access environment variables
-const edamamAppId = process.env.API_ID;  
-const edamamAppKey = process.env.API_KEY;  
+const edamamAppId = process.env.API_ID;
+const edamamAppKey = process.env.API_KEY;
 
 // Define a route for handling the search API with both GET and POST methods
-app.route('/api/search')
-    .get(async (req, res) => {
-        const { term, ingredients } = req.query;
-        console.log('Search Term:', term);
-        console.log('Ingredients:', ingredients);
+app.route("/api/search").get(async (req, res) => {
+  const { term, ingredients } = req.query;
+  // Add the following debug lines - remove after
+  console.log('Query Parameters:', req.query);
+  console.log('Search Term:', term);
+  console.log('Ingredients:', ingredients);
 
-        try {
-            let apiUrl;
-            if (term) {
-                apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${encodeURIComponent(term)}&app_id=${edamamAppId}&app_key=${edamamAppKey}`;
-            } else if (ingredients) {
-                apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${encodeURIComponent(ingredients)}&app_id=${edamamAppId}&app_key=${edamamAppKey}`;
-            } else {
-                throw new Error('At least one of search term or ingredients must be provided.');
-            }
 
-            console.log('API URL:', apiUrl);
-            const edamamResponse = await fetch(apiUrl);
-            const edamamData = await edamamResponse.json();
-            console.log('Edamam Data:', edamamData);
+  try {
+    let apiUrl;
+    if (term) {
+      apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${encodeURIComponent(
+        term
+      )}&app_id=${edamamAppId}&app_key=${edamamAppKey}`;
+    } else if (ingredients) {
+      apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${encodeURIComponent(
+        ingredients
+      )}&app_id=${edamamAppId}&app_key=${edamamAppKey}`;
+    } else {
+      throw new Error(
+        "At least one of search term or ingredients must be provided."
+      );
+    }
 
-            res.status(200).json({ hits: edamamData.hits });
-        } catch (error) {
-            console.error('Error making Edamam API request:', error);
-            res.status(500).json({ error: 'Internal server error' });
-        }
-    });
+    console.log("API URL:", apiUrl);
+    const edamamResponse = await fetch(apiUrl);
+    const edamamData = await edamamResponse.json();
+    console.log("Edamam Data:", edamamData);
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+    res.status(200).json({ hits: edamamData.hits });
+  } catch (error) {
+    console.error("Error making Edamam API request:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
+  console.log(`Server is listening at http://localhost:${port}`);
 });
--- public/search.js
+++ public/search.js
@@ -1,76 +1,102 @@
-const searchInput = document.getElementById('searchInput');
-const ingredientsInput = document.getElementById('ingredientsInput');
-const searchResultsContainer = document.getElementById('searchResults');
-const performSearchButton = document.getElementById('performSearch');
+const searchInput = document.getElementById("searchInput");
+const ingredientsInput = document.getElementById("ingredientsInput");
+const categoryDropdown = document.getElementById("category");
+const selectionsContainer = document.getElementById("selectionsContainer");
+const searchResultsContainer = document.getElementById("searchResults");
+const performSearchButton = document.getElementById("performSearch");
 
 // Attach the event listener to the search button
-performSearchButton.addEventListener('click', () => {
-    performSearch();
+performSearchButton.addEventListener("click", () => {
+  performSearch();
 });
 
 function performSearch() {
-    const searchTerm = searchInput.value;
-    const ingredients = ingredientsInput.value;
+  const searchTerm = searchInput.value;
+  const ingredients = ingredientsInput.value;
+  const category = categoryDropdown.value;
+  let selections;
 
-    // Check if either searchTerm or ingredients is provided
-    if (!searchTerm && !ingredients) {
-        console.error('At least one of search term or ingredients must be provided.');
-        return;
-    }
+  // Check if either searchTerm, ingredients, or category with selection is provided
+  if (!(searchTerm || ingredients || (category && selections))) {
+    console.error(
+      "At least one of search term, ingredients, or category with selection must be provided."
+    );
+    return;
+  } else if (category === "ingredients") {
+    // For ingredients category, get the input value
+    selections = ingredients;
+  } else if (category && selections) {
+    // For other categories, get the selected option value
+    selections = selectionsContainer.querySelector("select").value;
+  }
 
-    // Construct the API URL based on the available inputs
-    let apiUrl = 'http://localhost:3000/api/search?';
-    if (searchTerm) {
-        apiUrl += `term=${searchTerm}`;
-    }
-    if (ingredients) {
-        apiUrl += `${searchTerm ? '&' : ''}ingredients=${ingredients}`;
-    }
+  //These are for testing only - remove when testing complete
+  console.log("Search Term:", searchTerm);
+  console.log("Ingredients:", ingredients);
+  console.log("Category:", category);
+  console.log("Selections:", selections);
 
-    // Make the API request
-    fetch(apiUrl, {
-        method: 'GET',
+  // Construct the API URL based on the available inputs
+  let apiUrl = "http://localhost:3000/api/search?";
+
+  if (searchTerm) {
+    apiUrl += `term=${encodeURIComponent(searchTerm)}`;
+  }
+
+  if (ingredients) {
+    apiUrl += `${searchTerm ? "&" : ""}ingredients=${encodeURIComponent(
+      ingredients
+    )}`;
+  } else if (category === "ingredients" && selections) {
+    apiUrl += `ingredients=${encodeURIComponent(selections)}&`;
+  } else if (category && selections) {
+    apiUrl += `category=${category}&selections=${encodeURIComponent(
+      selections
+    )}&`;
+  }
+
+  // Make the API request
+  fetch(apiUrl, {
+    method: "GET",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
     })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data.hits);
-            displayResults(data.hits);
-        })
-        .catch(error => console.error('Error:', error));
+    .then((data) => {
+      console.log(data.hits);
+      displayResults(data.hits);
+    })
+    .catch((error) => console.error("Error:", error));
 }
 
 function displayResults(results) {
-    // Clear previous results
-    searchResultsContainer.innerHTML = '';
+  // Clear previous results
+  searchResultsContainer.innerHTML = "";
 
-    if (!results || results.length === 0) {
-        searchResultsContainer.innerHTML = 'No results found.';
-    } else {
-        const ul = document.createElement('ul');
-        results.forEach(function (result) {
-            const li = document.createElement('li');
+  if (!results || results.length === 0) {
+    searchResultsContainer.innerHTML = "No results found.";
+  } else {
+    const ul = document.createElement("ul");
+    results.forEach(function (result) {
+      const li = document.createElement("li");
 
-            // Create a link for each recipe
-            const link = document.createElement('a');
-            link.textContent = result.recipe.label;
+      // Create a link for each recipe
+      const link = document.createElement("a");
+      link.textContent = result.recipe.label;
 
-            // Extract the recipe ID from the URI
-            const recipeId = result.recipe.uri.split('_')[1];
-            
-            // Construct the link using the recipe ID
-            link.href = result.recipe.url;
-            link.target = '_blank';  // Open link in a new tab
+      // Extract the recipe ID from the URI
+      const recipeId = result.recipe.uri.split("_")[1];
 
-            li.appendChild(link);
-            ul.appendChild(li);
-        });
-        searchResultsContainer.appendChild(ul);
-    }
+      // Construct the link using the recipe ID
+      link.href = result.recipe.url;
+      link.target = "_blank"; // Open link in a new tab
+
+      li.appendChild(link);
+      ul.appendChild(li);
+    });
+    searchResultsContainer.appendChild(ul);
+  }
 }
-
-
