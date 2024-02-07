4a38e3ec8fcf63f70bd4b970ae5cf0298ca5e04a
Jennifer Larsen <djlarsen1999@gmail.com>
2024-02-06 23:42:37 -0600

    Connecting to Database

--- justConnect.js
+++ justConnect.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { MongoClient } = require("mongodb");
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.TY_DB;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
