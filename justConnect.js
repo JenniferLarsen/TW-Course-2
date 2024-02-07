dd37be6fc13c7e19fb6cb238c3d2fc15d8e14746
Jennifer Larsen <djlarsen1999@gmail.com>
2024-02-06 23:20:05 -0600

    added MONGODB_URI to justConnect.js

--- justConnect.js
+++ justConnect.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { MongoClient } = require("mongodb");
 
-const uri = process.env.DB_URI;
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
