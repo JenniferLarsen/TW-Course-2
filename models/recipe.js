e6a3c0de5fa4aac3357ed8f1e059e3b0738e9c66
Jennifer Larsen <djlarsen1999@gmail.com>
2024-02-19 16:40:27 -0600

    still connecting links and favorites buttons to database

--- models/recipe.js
+++ models/recipe.js
@@ -5,11 +5,7 @@ const recipeSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
+  }
 });
 
 const FavoriteRecipe = mongoose.model('FavoriteRecipe', recipeSchema);
