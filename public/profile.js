e2e2fe49efca8a1d48e7877804aa8d716bb56ad7
Jennifer Larsen <djlarsen1999@gmail.com>
2024-02-24 20:17:03 -0600

    Updates done - Freezing Code until after presentation.

--- public/profile.js
+++ public/profile.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     userGreeting.textContent = `Welcome Back, ${userData.username}!`;
   } else {
     // Handle the case when there's no user data (user not logged in)
-    userGreeting.textContent = "Welcome Back!";
+    userGreeting.textContent = "Welcome!";
   }
 
   // Prep before displaying
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const likesHtml = document.getElementById("likes_section");
   const favesHtml = document.getElementById("faves_section");
   // console.log(container);
-   console.log(tabs);
+  console.log(tabs);
   // console.log(selected_tab);
   // console.log(liked_items);
   // console.log(fav_items);
@@ -53,12 +53,12 @@ document.addEventListener("DOMContentLoaded", async () => {
     //htmlElement.innerHTML = "";
     let favesRow = document.createElement("div");
     for (let i = 0; i < list.length; i++) {
-    //    if (list.length % 3 === 0) {
-        likesRow = document.createElement("div");
-        htmlElement.appendChild(likesRow);
-        favesRow = document.createElement("div");
-        htmlElement.appendChild(favesRow);
-       //}
+      //    if (list.length % 3 === 0) {
+      likesRow = document.createElement("div");
+      htmlElement.appendChild(likesRow);
+      favesRow = document.createElement("div");
+      htmlElement.appendChild(favesRow);
+      //}
       let buildURI = "/api/id-search?";
       // list.forEach(recipeID => {
       const recipeID = list[i];
@@ -77,31 +77,43 @@ document.addEventListener("DOMContentLoaded", async () => {
         .then((data) => {
           console.log(data);
           const elem = document.createElement("div");
-          
 
-          //add an element for image
-          const image = document.createElement("img");
-          elem.appendChild(image);
-          image.src = data.hit.recipe.image;
-          
-          // add an element for url
-          const url = document.createElement("a");
-          //elem.appendChild(url);
-          //const link = document.createTextNode("Click for Recipe");
-          //url.appendChild(link);
-          url.title = "Recipe Link";
-          url.href = data.hit.recipe.url;
-          url.target = "_blank";
-
-          // add an element for label
           const label = document.createElement("p");
-          label.appendChild(url);
           elem.appendChild(label);
           label.classList.add("meal-name");
           label.id = "meal-name";
           label.title = data.hit.recipe.label;
-          label.textContent = data.hit.recipe.label;
 
+          // Make the label a clickable link
+          label.innerHTML = `<a href="${data.hit.recipe.url}" target="_blank" title="Recipe Link">${data.hit.recipe.label}</a>`;
+
+          // add an element for image
+          const image = document.createElement("img");
+          elem.appendChild(image);
+          image.src = data.hit.recipe.image;
+
+          // //add an element for image
+          // const image = document.createElement("img");
+          // elem.appendChild(image);
+          // image.src = data.hit.recipe.image;
+
+          // // add an element for url
+          // const url = document.createElement("a");
+          // elem.appendChild(url);
+          // const link = document.createTextNode("Click for Recipe");
+          // url.appendChild(link);
+          // url.title = "Recipe Link";
+          // url.href = data.hit.recipe.url;
+          // url.target = "_blank";
+
+          // // add an element for label
+          // const label = document.createElement("p");
+          // label.appendChild(url);
+          // elem.appendChild(label);
+          // label.classList.add("meal-name");
+          // label.id = "meal-name";
+          // label.title = data.hit.recipe.label;
+          // label.textContent = data.hit.recipe.label;
 
           // add the items to the elem
           likesRow.appendChild(elem);
@@ -121,74 +133,80 @@ document.addEventListener("DOMContentLoaded", async () => {
   // Change selected tab on click
   /** iterate the tabs and assign them to variables that correspond to their innerhtml.toLowerCase(); */
 
-  
   // const cbox = document.querySelectorAll(".box");
-  
+
   //  for (let i = 0; i < cbox.length; i++) {
-      //      cbox[i].addEventListener("click", function() {
-          //        cbox[i].classList.toggle("red");
-          //      });
-          //  }
-          const lkTab = tabs.item(0);
-          const fvTab = tabs.item(1);
-          console.log(lkTab);
-          console.log(fvTab);
+  //      cbox[i].addEventListener("click", function() {
+  //        cbox[i].classList.toggle("red");
+  //      });
+  //  }
+  const lkTab = tabs.item(0);
+  const fvTab = tabs.item(1);
+  console.log(lkTab);
+  console.log(fvTab);
 
-        //   const pending = document.createTextNode(". . .Brewing in the Kitchen . . .");
-        //   let st = `{.hide {visibility: "hidden";}`;
-        // //   pending.classList.add("brew");
-        //   pending.insertRule(st,0);
+  //   const pending = document.createTextNode(". . .Brewing in the Kitchen . . .");
+  //   let st = `{.hide {visibility: "hidden";}`;
+  // //   pending.classList.add("brew");
+  //   pending.insertRule(st,0);
 
-    tabs.forEach( tab => {
-        tab.onclick = (e) => {
-            console.log(e.target);
-            console.log("clicked tab");
+  tabs.forEach((tab) => {
+    tab.onclick = (e) => {
+      console.log(e.target);
+      console.log("clicked tab");
 
-            // Determine Tab and Display Content
-            if(e.target == lkTab){
-                console.log("liked tab if...");
-                fvTab.classList.contains("selected")? fvTab.classList.replace("selected", "unselected"): fvTab.classList.add("unselected");
-                favesHtml.classList.remove("show");
+      // Determine Tab and Display Content
+      if (e.target == lkTab) {
+        console.log("liked tab if...");
+        fvTab.classList.contains("selected")
+          ? fvTab.classList.replace("selected", "unselected")
+          : fvTab.classList.add("unselected");
+        favesHtml.classList.remove("show");
 
-                lkTab.classList.contains("unselected")?lkTab.classList.replace("unselected", "selected"): lkTab("selected") ;
-                //lkTab.classList.add("selected");
-                likesHtml.classList.add("show");
+        lkTab.classList.contains("unselected")
+          ? lkTab.classList.replace("unselected", "selected")
+          : lkTab("selected");
+        //lkTab.classList.add("selected");
+        likesHtml.classList.add("show");
 
-                // pending.classList.replace("show", "hide");
-                 container.appendChild(likesHtml);
-            }
-            else if(e.target == fvTab){
-                console.log("fave tab if...");
-                lkTab.classList.contains("selected") ? lkTab.classList.replace("selected", "unselected") : lkTab.classList.add("unselected");
-                likesHtml.classList.remove("show");
+        // pending.classList.replace("show", "hide");
+        container.appendChild(likesHtml);
+      } else if (e.target == fvTab) {
+        console.log("fave tab if...");
+        lkTab.classList.contains("selected")
+          ? lkTab.classList.replace("selected", "unselected")
+          : lkTab.classList.add("unselected");
+        likesHtml.classList.remove("show");
 
-                fvTab.classList.contains("unselected") ? fvTab.classList.replace("unselected", "selected") : fvTab.classList.add("selected") ;
-                favesHtml.classList.add("show");
-                //lkTab.classList.add("selected");
-                // lkTab.classList.replace("selected", "unselected");
-                // likesHtml.classList.remove("show");
-                
-                // fvTab.classList.replace("unselected", "selected");
-                // fvTab.classList.add("selected");
+        fvTab.classList.contains("unselected")
+          ? fvTab.classList.replace("unselected", "selected")
+          : fvTab.classList.add("selected");
+        favesHtml.classList.add("show");
+        //lkTab.classList.add("selected");
+        // lkTab.classList.replace("selected", "unselected");
+        // likesHtml.classList.remove("show");
 
-                // pending.classList.replace("show", "hide");
-                 container.appendChild(favesHtml);
-            }else{
-                // Nothing //
-            }
-        }
-    });   
-        // tab.classlist.add("selected-tab");
-        
-        // if(tab.innerHTML == "Likes"){
-         //container.appendChild(likesHtml);
-        //     tab.classList.add("show");
+        // fvTab.classList.replace("unselected", "selected");
+        // fvTab.classList.add("selected");
 
-        // }else if(tab.innerHTML == "Favorites"){
-        //     container.appendChild(favesHtml);
-        // }else{
-            //container.appendChild("Build Your Personal Cookbook Here!");
-        //}
+        // pending.classList.replace("show", "hide");
+        container.appendChild(favesHtml);
+      } else {
+        // Nothing //
+      }
+    };
+  });
+  // tab.classlist.add("selected-tab");
+
+  // if(tab.innerHTML == "Likes"){
+  //container.appendChild(likesHtml);
+  //     tab.classList.add("show");
+
+  // }else if(tab.innerHTML == "Favorites"){
+  //     container.appendChild(favesHtml);
+  // }else{
+  //container.appendChild("Build Your Personal Cookbook Here!");
+  //}
 });
 
 /** search by ID */
